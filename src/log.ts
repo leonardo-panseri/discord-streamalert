82d198ba889333cb671f5bb50f4ec52778295a14
Leonardo Panseri <leonardo.panseri@gmail.com>
2022-02-03 18:42:57 +0100

    add timestamp to logs

--- src/log.ts
+++ src/log.ts
@@ -2,13 +2,16 @@ import { createLogger, format, transports } from 'winston';
 
 const logger = createLogger({
     level: process.env.DEBUG ? 'debug' : 'info',
-    format: format.printf(options => {
-        if (options.moduleName) {
-            return `[${options.moduleName}] ${options.level}: ${options.message}`;
-        } else {
-            return `[Main] ${options.level}: ${options.message}`;
-        }
-    }),
+    format: format.combine(
+        format.timestamp({ format: 'MM-DD hh:mm:ss' }),
+        format.printf(options => {
+            if (options.moduleName) {
+                return `${options.timestamp} [${options.moduleName}] ${options.level}: ${options.message}`;
+            } else {
+                return `${options.timestamp} [Main] ${options.level}: ${options.message}`;
+            }
+        })
+    ),
     transports: [
         new transports.Console(),
     ],
